98638b6cb38417f84a9d949773fc5b740a8ec630
Julian Maicher <julian@inventorum.com>
2015-04-01 16:55:36 +0200

    Add popo serializer with tests

---
+++ src/inventorum.ebay/inventorum/ebay/lib/rest/tests/test_popo_serializer.py
@@ -0,0 +1,186 @@
+# encoding: utf-8
+from __future__ import absolute_import, unicode_literals
+import logging
+
+from decimal import Decimal as D
+from datetime import date
+
+from unittest.case import TestCase
+
+from inventorum.ebay.lib.rest.serializers import POPOSerializer
+
+from rest_framework import serializers
+
+
+log = logging.getLogger(__name__)
+
+
+class _Customer(object):
+    def __init__(self, first_name, last_name, birthday=None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birthday = birthday
+
+
+class _CustomerSerializer(POPOSerializer):
+
+    class Meta:
+        model = _Customer
+
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    BirthDay = serializers.DateField(source="birthday", required=False)
+
+
+class _CustomerSerializerWithUndefinedPOPOAttributes(_CustomerSerializer):
+    undefined_popo_attribute = serializers.IntegerField()
+
+
+class _CustomerSerializerWithUndefinedSerializerAttributes(POPOSerializer):
+
+    class Meta:
+        model = _Customer
+
+
+class _Order(object):
+    def __init__(self, items):
+        self.items = items
+
+
+class _OrderItem(object):
+    def __init__(self, product_id, price, quantity):
+        self.product_id = product_id
+        self.price = price
+        self.quantity = quantity
+
+
+class _OrderItemSerializer(POPOSerializer):
+
+    class Meta:
+        model = _OrderItem
+
+    product_id = serializers.IntegerField()
+    price = serializers.DecimalField(max_digits=10, decimal_places=2)
+    quantity = serializers.IntegerField()
+
+
+class _OrderSerializer(POPOSerializer):
+
+    class Meta:
+        model = _Order
+
+    items = _OrderItemSerializer(many=True)
+
+
+class TestPOPOSerializer(TestCase):
+
+    def test_serializes_simple_popo(self):
+        customer = _Customer("John", "Wayne")
+        serializer = _CustomerSerializer(customer)
+
+        self.assertEqual(serializer.data, {
+            "first_name": "John",
+            "last_name": "Wayne",
+            "BirthDay": None
+        })
+
+        customer = _Customer("John", "Wayne", date(2015, 04, 01))
+        serializer = _CustomerSerializer(customer)
+
+        self.assertEqual(serializer.data, {
+            "first_name": "John",
+            "last_name": "Wayne",
+            "BirthDay": "2015-04-01"
+        })
+
+    def test_deserializes_simple_popo(self):
+        data = {
+            "first_name": "John",
+            "last_name": "Wayne"
+        }
+
+        serializer = _CustomerSerializer(data=data)
+
+        instance = serializer.build()
+        self.assertIsInstance(instance, _Customer)
+        self.assertEqual(instance.first_name, "John")
+        self.assertEqual(instance.last_name, "Wayne")
+        self.assertEqual(instance.birthday, None)
+
+        data.update({
+            "BirthDay": "2015-04-01"
+        })
+
+        serializer = _CustomerSerializer(data=data)
+        instance = serializer.build()
+        self.assertTrue(instance)
+        self.assertEqual(instance.birthday, date(2015, 04, 01))
+
+    def test_serializes_nested_popos(self):
+        items = [
+            _OrderItem(1, price=D("3.99"), quantity=5),
+            _OrderItem(2, price=D("1.00"), quantity=3)
+        ] 
+        order = _Order(items)
+        
+        serializer = _OrderSerializer(order)
+
+        self.assertEqual(serializer.data, {
+            "items": [
+                {
+                    "product_id": 1,
+                    "price": "3.99",
+                    "quantity": 5
+                },
+                {
+                    "product_id": 2,
+                    "price": "1.00",
+                    "quantity": 3
+                }
+            ]
+        })
+
+    def test_deserializes_nested_popos(self):
+        data = {
+            "items": [
+                {
+                    "product_id": 23,
+                    "price": "0.49",
+                    "quantity": 1
+                },
+                {
+                    "product_id": 42,
+                    "price": "100.00",
+                    "quantity": 4
+                }
+            ]
+        }
+
+        serializer = _OrderSerializer(data=data)
+
+        instance = serializer.build()
+        self.assertIsInstance(instance, _Order)
+        self.assertEqual(len(instance.items), 2)
+        for item in instance.items:
+            self.assertIsInstance(item, _OrderItem)
+
+    def test_deserialize_throws_with_additional_attributes(self):
+        data = {
+            "first_name": "John",
+            "last_name": "Wayne",
+            "undefined_popo_attribute": 1
+        }
+
+        serializer = _CustomerSerializerWithUndefinedPOPOAttributes(data=data)
+        with self.assertRaises(TypeError):
+            serializer.build()
+
+    def test_deserialize_throws_with_too_few_serializer_attributes(self):
+        data = {
+            "first_name": "John",
+            "last_name": "Wayne",
+        }
+
+        serializer = _CustomerSerializerWithUndefinedSerializerAttributes(data=data)
+        with self.assertRaises(TypeError):
+            serializer.build()
