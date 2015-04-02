8698c3f4a37d378303c30e9ef35c2f83390ba6c5
Julian Maicher <julian@inventorum.com>
2015-04-02 17:37:55 +0200

    Add basic skeleton for publishing with core api fetch

--- src/inventorum.ebay/inventorum/ebay/apps/products/resources.py
+++ src/inventorum.ebay/inventorum/ebay/apps/products/resources.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals
 import logging
 
 from inventorum.ebay.lib.rest.resources import APIResource
+from requests.exceptions import HTTPError
 from rest_framework.response import Response
 
 
@@ -12,4 +13,19 @@ log = logging.getLogger(__name__)
 class PublishResource(APIResource):
 
     def post(self, request, inv_product_id):
-        return Response()
+        try:
+            core_product = request.user.core_api.get_product(inv_product_id)
+        except HTTPError as e:
+            return Response(status=e.response.status_code)
+
+        return Response(data=core_product.name)
+
+        # TODO:
+        # 1. sync: aggregate data, construct "ebay listing" (take from core product and ebay service)
+        # 2. sync: validate "ebay listing"
+        #   - is not already published
+        #   - has billing address
+        #   - has shipping services
+        #   - has price >= 1
+        #   - has category
+        # 3. async: publish ebay listing
--- src/inventorum.ebay/inventorum/ebay/apps/products/tests/test_product_publish.py
+++ src/inventorum.ebay/inventorum/ebay/apps/products/tests/test_product_publish.py
@@ -1,7 +1,9 @@
 # encoding: utf-8
 from __future__ import absolute_import, unicode_literals
 import logging
+from inventorum.ebay.apps.core_api.tests import CoreApiTest
 from inventorum.ebay.apps.products.models import EbayProductModel
+from inventorum.ebay.tests import StagingTestAccount
 
 from inventorum.ebay.tests.testcases import APITestCase
 
@@ -11,9 +13,12 @@ log = logging.getLogger(__name__)
 
 class TestProductPublish(APITestCase):
 
+    @CoreApiTest.vcr.use_cassette("get_product_simple.json")
     def test_first_time_publish(self):
-        inv_product_id = 1
+        inv_product_id = StagingTestAccount.Products.SIMPLE_PRODUCT_ID
         assert not EbayProductModel.objects.by_inv_id(inv_product_id).exists()
 
         response = self.client.post("/products/%s/publish" % inv_product_id)
         self.assertEqual(response.status_code, 200)
+        # core api product name, just to prove that the connection is working
+        self.assertEqual(response.data, "XtC Advanced 2 LTD")
