ca9cbe0ddd175fb55065b27ce01303f7c60db748
Julian Maicher <julian@inventorum.com>
2015-04-26 22:40:16 +0200

    Remove event consts

--- src/inventorum.ebay/inventorum/ebay/apps/notifications/__init__.py
+++ src/inventorum.ebay/inventorum/ebay/apps/notifications/__init__.py
@@ -6,21 +6,3 @@ import logging
 log = logging.getLogger(__name__)
 
 
-class NotificationEventTypeCodeType(object):
-    """
-    Platform notifications that can be sent by ebay and are handled/supported by our system
-
-    http://developer.ebay.com/Devzone/XML/docs/Reference/ebay/types/NotificationEventTypeCodeType.html
-    """
-
-    # Sent each time a buyer purchases an item in a multiple-quantity, fixed-price listing
-    FixedPriceTransaction = "FixedPriceTransaction"
-
-    # Sent instead of 'FixedPriceTransaction' if a single-quantity, fixed-price listing is purchased
-    ItemSold = "ItemSold"
-
-    # Sent if a fixed-price listing ends with no sales
-    ItemClosed = "ItemClosed"
-
-    # Sent if eBay has administratively ended a listing for whatever reason
-    ItemSuspended = "ItemSuspended"
