73ef21a3b05cce4ba0e73c1fc0c428badce5e405
Julian Maicher <julian@inventorum.com>
2015-04-27 16:05:13 +0200

    Add ebay notification model

--- src/inventorum.ebay/inventorum/ebay/lib/ebay/notifications.py
+++ src/inventorum.ebay/inventorum/ebay/lib/ebay/notifications.py
@@ -1,42 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 import logging
-
 import base64
 import hashlib
 import re
 import datetime
 
 from ebaysdk.response import ResponseDataObject, Response
+from inventorum.ebay.lib.rest.serializers import POPOSerializer
 
 
 log = logging.getLogger(__name__)
-
-
-class EbayNotificationEventType(object):
-    """
-    A supported subset of the platform notifications that can be sent by ebay
-
-    http://developer.ebay.com/Devzone/XML/docs/Reference/ebay/types/EbayNotificationEventType.html
-    """
-    # Sent each time a buyer purchases an item in a multiple-quantity, fixed-price listing
-    FixedPriceTransaction = "FixedPriceTransaction"
-
-    # Sent instead of 'FixedPriceTransaction' if a single-quantity, fixed-price listing is purchased
-    ItemSold = "ItemSold"
-
-    # Sent if a fixed-price listing ends with or without any sales
-    ItemClosed = "ItemClosed"
-
-    # Sent if eBay has administratively ended a listing for whatever reason
-    ItemSuspended = "ItemSuspended"
-
-    CHOICES = (
-        (FixedPriceTransaction, "FixedPriceTransaction"),
-        (ItemSold, "ItemSold"),
-        (ItemClosed, "ItemClosed"),
-        (ItemSuspended, "ItemSuspended")
-    )
 
 
 class EbayNotification(object):
