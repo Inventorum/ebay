3232a7239d7fd09971f9682d188bc757adb0aaed
Julian Maicher <julian@inventorum.com>
2015-04-29 17:11:56 +0200

    Extend order model with shipping, regenerate migrations

--- src/inventorum.ebay/inventorum/ebay/apps/orders/migrations/0001_initial.py
+++ src/inventorum.ebay/inventorum/ebay/apps/orders/migrations/0001_initial.py
@@ -12,19 +12,22 @@ import inventorum.util.django.db.models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contenttypes', '0001_initial'),
+        ('accounts', '0006_ebayaccountmodel_last_core_api_sync'),
         ('shipping', '0004_auto_20150429_0036'),
+        ('contenttypes', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='OrderLineItem',
+            name='OrderLineItemModel',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('time_added', models.DateTimeField(auto_now_add=True, verbose_name='Created at')),
                 ('time_modified', models.DateTimeField(default=django.utils.timezone.now, verbose_name='Last change', auto_now=True)),
                 ('is_active', models.BooleanField(default=True, verbose_name='Is active')),
                 ('deleted_at', models.DateTimeField(default=datetime.datetime(1970, 1, 1, 0, 0, tzinfo=utc), verbose_name='Time of deletion')),
+                ('inv_id', models.IntegerField(unique=True, null=True, verbose_name='Universal inventorum id', blank=True)),
+                ('ebay_id', models.CharField(max_length=255, verbose_name='Ebay transaction id')),
                 ('orderable_item_id', models.PositiveIntegerField()),
                 ('quantity', models.PositiveIntegerField(verbose_name='Quantity')),
                 ('unit_price', inventorum.ebay.lib.db.fields.MoneyField(verbose_name='Unit price', max_digits=10, decimal_places=2)),
@@ -44,10 +47,12 @@ class Migration(migrations.Migration):
                 ('deleted_at', models.DateTimeField(default=datetime.datetime(1970, 1, 1, 0, 0, tzinfo=utc), verbose_name='Time of deletion')),
                 ('inv_id', models.IntegerField(unique=True, null=True, verbose_name='Universal inventorum id', blank=True)),
                 ('ebay_id', models.CharField(max_length=255, verbose_name='Ebay id')),
-                ('total_price', inventorum.ebay.lib.db.fields.MoneyField(verbose_name='Total price incl. shipping', max_digits=10, decimal_places=2)),
-                ('created_from_id', models.PositiveIntegerField()),
-                ('created_from_type', models.ForeignKey(to='contenttypes.ContentType')),
-                ('shipping_service', models.OneToOneField(related_name='order', null=True, blank=True, to='shipping.ShippingServiceConfigurationModel')),
+                ('final_price', inventorum.ebay.lib.db.fields.MoneyField(verbose_name='Final price on ebay', max_digits=10, decimal_places=2)),
+                ('ebay_status', models.CharField(max_length=255, choices=[('Complete', 'Complete'), ('Incomplete', 'Incomplete'), ('Pending', 'Pending')])),
+                ('created_from_id', models.PositiveIntegerField(null=True, blank=True)),
+                ('account', models.ForeignKey(verbose_name='Ebay account', to='accounts.EbayAccountModel')),
+                ('created_from_type', models.ForeignKey(blank=True, to='contenttypes.ContentType', null=True)),
+                ('shipping', models.OneToOneField(related_name='order', null=True, blank=True, to='shipping.ShippingServiceConfigurationModel')),
             ],
             options={
                 'abstract': False,
@@ -55,13 +60,13 @@ class Migration(migrations.Migration):
             bases=(inventorum.util.django.db.models.ModelMixins, models.Model),
         ),
         migrations.AddField(
-            model_name='orderlineitem',
+            model_name='orderlineitemmodel',
             name='order',
             field=models.ForeignKey(related_name='line_items', verbose_name='Order', to='orders.OrderModel'),
             preserve_default=True,
         ),
         migrations.AddField(
-            model_name='orderlineitem',
+            model_name='orderlineitemmodel',
             name='orderable_item_type',
             field=models.ForeignKey(to='contenttypes.ContentType'),
             preserve_default=True,
--- src/inventorum.ebay/inventorum/ebay/apps/orders/models.py
+++ src/inventorum.ebay/inventorum/ebay/apps/orders/models.py
@@ -8,13 +8,14 @@ from inventorum.ebay.lib.db.fields import MoneyField
 
 from inventorum.ebay.lib.db.models import BaseModel, MappedInventorumModelQuerySet
 from django.db import models
+from inventorum.ebay.lib.ebay.data import CompleteStatusCodeType
 from inventorum.util.django.model_utils import PassThroughManager
 
 
 log = logging.getLogger(__name__)
 
 
-class OrderLineItem(BaseModel):
+class OrderLineItemModel(BaseModel):
     """
     An order line item (a line item within an order) is the record of one buyer's commitment to purchase one or more
     *identical* items from the same listing.
@@ -25,6 +26,9 @@ class OrderLineItem(BaseModel):
     """
     order = models.ForeignKey("orders.OrderModel", verbose_name="Order", related_name="line_items")
 
+    inv_id = models.IntegerField(unique=True, null=True, blank=True, verbose_name="Universal inventorum id")
+    ebay_id = models.CharField(max_length=255, verbose_name="Ebay transaction id")
+
     # Generic reference to an orderable item (either `EbayItemModel` or `EbayItemVariationModel`)
     orderable_item_type = models.ForeignKey(ContentType)
     orderable_item_id = models.PositiveIntegerField()
@@ -33,25 +37,33 @@ class OrderLineItem(BaseModel):
     quantity = models.PositiveIntegerField(verbose_name="Quantity")
     unit_price = MoneyField(verbose_name="Unit price")
 
+    @property
+    def inv_product_id(self):
+        return self.orderable_item.inv_product_id
+
 
 class OrderModelQuerySet(MappedInventorumModelQuerySet):
     pass
 
 
 class OrderModel(BaseModel):
+    account = models.ForeignKey("accounts.EbayAccountModel", verbose_name="Ebay account")
+
     inv_id = models.IntegerField(unique=True, null=True, blank=True, verbose_name="Universal inventorum id")
     ebay_id = models.CharField(max_length=255, verbose_name="Ebay id")
 
-    # total price as computed by ebay
-    total_price = MoneyField(verbose_name="Total price incl. shipping")
+    # final price as computed by ebay including shipping and anything else
+    final_price = MoneyField(verbose_name="Final price on ebay")
 
-    shipping_service = models.OneToOneField("shipping.ShippingServiceConfigurationModel", null=True, blank=True,
-                                            related_name="order")
+    shipping = models.OneToOneField("shipping.ShippingServiceConfigurationModel", null=True, blank=True,
+                                    related_name="order")
+
+    ebay_status = models.CharField(max_length=255, choices=CompleteStatusCodeType.CHOICES)
 
     # Generic reference that allows us to track from what/where this order has been created
     # For instance, ``created_from`` could refer to an FixedPriceTransaction notification
-    created_from_type = models.ForeignKey(ContentType)
-    created_from_id = models.PositiveIntegerField()
+    created_from_type = models.ForeignKey(ContentType, null=True, blank=True)
+    created_from_id = models.PositiveIntegerField(null=True, blank=True)
     created_from = GenericForeignKey('created_from_type', 'created_from_id')
 
     objects = PassThroughManager.for_queryset_class(OrderModelQuerySet)()
@@ -70,6 +82,16 @@ class OrderableItemModel(models.Model):
     class Meta:
         abstract = True
 
-    order_line_items = GenericRelation("orders.OrderLineItem",
-                                       content_type_field="orderable_type",
-                                       object_id_field="orderable_id")
+    order_line_items = GenericRelation("orders.OrderLineItemModel",
+                                       content_type_field="orderable_item_type",
+                                       object_id_field="orderable_item_id")
+
+    @property
+    def inv_product_id(self):
+        """
+        Must be defined by implementing classes either through a property or a model field
+        (http://stackoverflow.com/questions/1151212/equivalent-of-notimplementederror-for-fields-in-python)
+
+        :rtype: int
+        """
+        raise NotImplementedError
