1091e7b126efc5549eb5afab6f88867c2039ba58
Julian Maicher <julian@inventorum.com>
2015-04-29 17:14:39 +0200

    Add core api data serializer to create orders with tests

---
+++ src/inventorum.ebay/inventorum/ebay/apps/orders/tests/test_core_api_data_serializers.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+import logging
+
+from decimal import Decimal as D
+from inventorum.ebay.apps.orders.serializers import OrderModelCoreAPIDataSerializer
+from inventorum.ebay.apps.orders.tests.factories import OrderModelFactory, OrderLineItemFactory
+from inventorum.ebay.apps.products.tests.factories import PublishedEbayItemFactory
+from inventorum.ebay.apps.shipping.models import ShippingServiceConfigurationModel
+from inventorum.ebay.apps.shipping.tests import ShippingServiceTestMixin
+
+from inventorum.ebay.tests.testcases import UnitTestCase
+
+
+log = logging.getLogger(__name__)
+
+
+class TestCoreAPIDataSerializers(UnitTestCase, ShippingServiceTestMixin):
+
+    def test_without_payment_and_shipping(self):
+        shipping = ShippingServiceConfigurationModel.objects.create(service=self.get_shipping_service_dhl(),
+                                                                    cost=D("4.50"))
+
+        order = OrderModelFactory.create(shipping=shipping)
+
+        published_ebay_item = PublishedEbayItemFactory(product__inv_id=23)
+        OrderLineItemFactory.create(order=order,
+                                    orderable_item=published_ebay_item,
+                                    quantity=5, unit_price=D("3.99"))
+
+        serializer = OrderModelCoreAPIDataSerializer(order)
+
+        self.assertDictEqual(serializer.data, {
+            "items": [{"product": 23, "quantity": 5, "gross_price": "3.99"}],
+            "shipment": {
+                "name": "DHL Paket",
+                "cost": "4.50",
+                "external_id": "DE_DHLPaket"
+            }
+        })
