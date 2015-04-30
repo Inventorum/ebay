0f106cccd9af2ffe88b02e4682c958cfcf5ee67b
Michał Hernas <michal@hernas.pl>
2015-04-30 14:31:14 +0200

    Fixed tests

--- src/inventorum.ebay/inventorum/ebay/__init__.py
+++ src/inventorum.ebay/inventorum/ebay/__init__.py
@@ -7,3 +7,14 @@ log = logging.getLogger(__name__)
 # This will make sure the celery app is always imported when
 # django starts so that shared_task will use this app.
 from .celery import app as celery_app
+
+import os
+
+ini = os.environ.get('PYRAMID_SETTINGS', None)
+if ini:
+    config_file, section_name = ini.split('#', 1)
+    from paste.deploy.loadwsgi import appconfig
+    import pyramid.paster
+
+    pyramid.paster.setup_logging(config_file)
+    app = pyramid.paster.get_app(config_file)
