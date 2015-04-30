a1ffe3ca49b7aeca5f42df30b581b569fd104a95
Bartosz Hernas <bartosz@hernas.pl>
2015-04-30 11:48:49 +0200

    Moved to services

--- src/inventorum.ebay/inventorum/ebay/apps/inventory/resources.py
+++ src/inventorum.ebay/inventorum/ebay/apps/inventory/resources.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import, unicode_literals
 import logging
 from inventorum.ebay.apps.inventory.serializers import SanityCheckEbaySerializer
+from inventorum.ebay.apps.inventory.services import CoreApiQuantityCheck
 from inventorum.ebay.apps.products.models import EbayItemModel
 from rest_framework.response import Response
 from inventorum.ebay.lib.rest.exceptions import BadRequest
@@ -9,30 +10,19 @@ from inventorum.ebay.lib.rest.resources import APIResource
 
 log = logging.getLogger(__name__)
 
+
 class SanityCheckResource(APIResource):
     serializer_class = SanityCheckEbaySerializer
+
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        data = serializer.instance
-        product_ids = []
-        ebay_items = {}
 
-        for index, availability in enumerate(data.availabilities):
-            product_id = EbayItemModel.clean_sku(availability.sku)
-            product_ids.append(product_id)
-            ebay_items[product_id] = availability  # to later access it easily
+        ebay_sanity_check = serializer.instance
 
-            if not availability.LocationID == request.user.account.ebay_location_id:
-                raise BadRequest(
-                    'Ebay\'s LocationID is not equal to currently authenticated account location id ("{0}" != "{1}")'.format(
-                        availability.LocationID, request.user.account.ebay_location_id))
-
-        core_api_quantity = request.user.core_api.get_quantity_info(product_ids)
-
-        for item in core_api_quantity:
-            ebay_items[item.id].quantity = int(item.quantity)
-
-        response_serializer = self.get_serializer(instance=data)
+        quantity_check = CoreApiQuantityCheck(ebay_sanity_check.availabilities, request.user.account.ebay_location_id,
+                                              request.user.core_api)
+        ebay_sanity_check.availabilities = quantity_check.refresh_quantities()
+        response_serializer = self.get_serializer(instance=ebay_sanity_check)
         return Response(data=response_serializer.data)
