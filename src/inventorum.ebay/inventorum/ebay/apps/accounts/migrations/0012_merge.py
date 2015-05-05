71c1fa698bfee94a7b8c946ac78531e5a5f1398e
Julian Maicher <julian@inventorum.com>
2015-05-05 20:30:53 +0200

    Merged conflicting migration

---
+++ src/inventorum.ebay/inventorum/ebay/apps/accounts/migrations/0012_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0011_auto_20150504_1707'),
+        ('accounts', '0011_auto_20150505_1531'),
+    ]
+
+    operations = [
+    ]
