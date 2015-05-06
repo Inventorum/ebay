2ac41fb29f7353f21ad00dbafc403542ae1b3dc3
Julian Maicher <julian@inventorum.com>
2015-05-06 16:19:57 +0200

    Add fn to get anonymous task execution context

--- src/inventorum.ebay/inventorum/ebay/lib/celery.py
+++ src/inventorum.ebay/inventorum/ebay/lib/celery.py
@@ -2,10 +2,18 @@
 from __future__ import absolute_import, unicode_literals
 import logging
 from django.test.utils import override_settings
+from inventorum.util.celery import TaskExecutionContext
 from nose.tools.nontrivial import make_decorator
 
 
 log = logging.getLogger(__name__)
+
+
+def get_anonymous_task_execution_context():
+    """
+    :rtype: inventorum.util.celery.TaskExecutionContext
+    """
+    return TaskExecutionContext(user_id=None, account_id=None, request_id=None)
 
 
 def celery_test_case(eager=True, propagate_exceptions=True):
