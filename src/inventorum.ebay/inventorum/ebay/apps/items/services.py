a0b3700774985e37a235a1451cf1b6be971b7af7
sandrakriemann <sandra@inventorum.com>
2015-08-25 19:26:16 +0200

    INV-5276: Moved everything related to items to one single directory

--- src/inventorum.ebay/inventorum/ebay/apps/items/services.py
+++ src/inventorum.ebay/inventorum/ebay/apps/items/services.py
@@ -5,20 +5,20 @@ log = logging.getLogger(__name__)
 
 
 class ImportProductFromEbayService(object):
-    log.info('Trying to import products from ebay')
 
     def __init__(self, account):
         """
         Checking if items ebay is asking for are still in stock
         :param account: account for what the ebay products can be imported
-        :type account: [inventorum.ebay.apps.accounts.models.EbayAccountModel]
+        :type account: inventorum.ebay.apps.accounts.models.EbayAccountModel
         """
         self.account = account
 
     def pull_data(self):
         """
-        wdqwd
-        :return:
+        Pulls items from ebay.
+        :type ebay: inventorum.ebay.apps.orders.models.OrderModel
         """
+        log.info('Trying to import products from ebay')
         ebay_api = EbayItems(self.account.token.ebay_object)
         items = ebay_api.get_items()
