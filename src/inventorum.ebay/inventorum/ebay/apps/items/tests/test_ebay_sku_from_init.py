182de7b766e3d951d83a37f8b3012f9054975e3f
sandrakriemann <sandra@inventorum.com>
2015-08-26 17:57:19 +0200

    added UnitTestsForEbaySKU

---
+++ src/inventorum.ebay/inventorum/ebay/apps/items/tests/test_ebay_sku_from_init.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import, unicode_literals
+from inventorum.ebay.apps.items import EbaySKU
+from inventorum.ebay.tests.testcases import UnitTestCase
+
+
+class UnitTestsForEbaySKU(UnitTestCase):
+
+    def setUp(self):
+        self.sku = 'invtest_1234'
+        self.wrong_sku = 'invstaging_1234'
+
+    def test_get_env_prefix(self):
+        self.assertEquals(EbaySKU.get_env_prefix(), 'invtest_')
+
+    def test_extract_product_id(self):
+        self.assertEqual(EbaySKU.extract_product_id(self.sku), '1234')
+
+    def test_belongs_to_current_env(self):
+        self.assertTrue(EbaySKU.belongs_to_current_env(self.sku))
+
+    def test_wrong_case_for_belongs_to_current_env(self):
+        self.assertFalse(EbaySKU.belongs_to_current_env(self.wrong_sku))
