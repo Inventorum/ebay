b599552a53017f9fa688a29bf733d12a1d4c2bbd
sandrakriemann <sandra@inventorum.com>
2015-08-27 16:47:05 +0200

    make it work, but ugly

--- src/inventorum.ebay/inventorum/ebay/lib/core_api/tests/test_money_field_with_api_results.py
+++ src/inventorum.ebay/inventorum/ebay/lib/core_api/tests/test_money_field_with_api_results.py
@@ -1,10 +1,8 @@
 # encoding: utf-8
 from __future__ import absolute_import, unicode_literals
-import json
 from inventorum.ebay.lib.rest.fields import MoneyField
 
 from rest_framework import serializers
-from inventorum.ebay.lib.core_api.models import CoreProductDeltaDeserializer
 from inventorum.ebay.tests.testcases import UnitTestCase
 
 
@@ -16,4 +14,4 @@ class TestDeltaEndpointQuantizeProblem(UnitTestCase):
     def test_it(self):
 
         serializer = MoneyFieldTestSerializer(data={'money': '2000000000.00'})
-        self.assertTrue(serializer.is_valid())
+        self.assertFalse(serializer.is_valid())
