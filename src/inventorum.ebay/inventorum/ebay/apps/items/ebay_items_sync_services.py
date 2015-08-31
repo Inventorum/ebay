acb9e6d142ca9e2c8449088fc8363828f65279e6
sandrakriemann <sandra@inventorum.com>
2015-08-31 18:02:15 +0200

    simplify class structure

--- src/inventorum.ebay/inventorum/ebay/apps/items/ebay_items_sync_services.py
+++ src/inventorum.ebay/inventorum/ebay/apps/items/ebay_items_sync_services.py
@@ -31,40 +31,6 @@ class EbayItemsSync(object):
             IncomingEbayItemSyncer(self.account, self.item).run()
 
 
-class EbayItemImporter(object):
-    """ Gets ebay_item and converts it to ebay_item_model -> store in database """
-
-    def __init__(self, account, item):
-        """
-        :type account: inventorum.ebay.apps.accounts.models.EbayAccountModel
-        """
-        self.sync = IncomingEbayItemSyncer(account, item)
-
-    @transaction.atomic()
-    def convert_to_ebay_item_model(self, ebay_item):
-        log.info(ebay_item)
-        item_model = EbayItemModel()
-        item_model.item_id = ebay_item.item_id
-        item_model.category = CategoryFactory()
-        item_model.description = ebay_item.description
-        item_model.postal_code = ebay_item.postal_code
-        item_model.ean = ebay_item.ean
-        item_model.is_click_and_collect = ebay_item.pick_up.is_eligible_for_pick_up
-        item_model.gross_price = ebay_item.start_price
-        item_model.quantity = ebay_item.quantity
-        item_model.name = ebay_item.title
-        item_model.inv_product_id = ebay_item.sku.split('_')[1]
-        item_model.account_id = self.sync.account.id
-
-        item_model.listing_duration = ebay_item.listing_duration
-        item_model.country = ebay_item.country
-        item_model.product = EbayProductFactory.create()
-        item_model.paypal_email_address = ebay_item.paypal_email_address
-        item_model.publishing_status = EbayItemPublishingStatus.PUBLISHED
-
-        item_model.save()
-
-
 class IncomingEbayItemSyncer(object):
     def __init__(self, account, item):
         """
@@ -81,7 +47,7 @@ class IncomingEbayItemSyncer(object):
         """
         if hasattr(self.item, 'sku') and self.item.sku is not None:
             if EbaySKU.belongs_to_current_env(self.item.sku):
-                self.start_importer_to_convert_to_ebay_item_model()
+                self.convert_to_ebay_item_model()
             else:
                 log.warning('There was an ebay item with another sku (not inventorum): %s', self.item.sku)
                 log.warning('No sku for item: %s Of accountId: %s', self.item.item_id, self.account.id)
@@ -90,7 +56,29 @@ class IncomingEbayItemSyncer(object):
             log.warning("Item was not created via Inventorum".format(self.item))
             log.warning('No sku for item: ' + str(self.item.item_id) + 'Of accountId: ' + str(self.account.id))
 
-    def start_importer_to_convert_to_ebay_item_model(self):
-        importer = EbayItemImporter(self.account, self.item)
-        importer.convert_to_ebay_item_model(self.item)
+    @transaction.atomic()
+    def convert_to_ebay_item_model(self):
+        """
+        Create database model for ebay item and stores it into db.
+        """
+        log.info(self.item)
+        item_model = EbayItemModel()
+        item_model.item_id = self.item.item_id
+        item_model.category = CategoryFactory()
+        item_model.description = self.item.description
+        item_model.postal_code = self.item.postal_code
+        item_model.ean = self.item.ean
+        item_model.is_click_and_collect = self.item.pick_up.is_eligible_for_pick_up
+        item_model.gross_price = self.item.start_price
+        item_model.quantity = self.item.quantity
+        item_model.name = self.item.title
+        item_model.inv_product_id = self.item.sku.split('_')[1]
+        item_model.account_id = self.account.id
 
+        item_model.listing_duration = self.item.listing_duration
+        item_model.country = self.item.country
+        item_model.product = EbayProductFactory.create()
+        item_model.paypal_email_address = self.item.paypal_email_address
+        item_model.publishing_status = EbayItemPublishingStatus.PUBLISHED
+
+        item_model.save()
