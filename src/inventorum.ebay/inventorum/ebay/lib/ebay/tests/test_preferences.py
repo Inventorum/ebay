55eaaebf4ed008f2022db1875973dd6a04f63b6c
Julian Maicher <julian@inventorum.com>
2015-09-15 19:36:43 +0200

    Extract data objects into lib.ebay.data

--- src/inventorum.ebay/inventorum/ebay/lib/ebay/tests/test_preferences.py
+++ src/inventorum.ebay/inventorum/ebay/lib/ebay/tests/test_preferences.py
@@ -1,8 +1,9 @@
 # encoding: utf-8
 from __future__ import absolute_import, unicode_literals
 import logging
+from inventorum.ebay.lib.ebay.data import SellerProfileCodeType
 
-from inventorum.ebay.lib.ebay.preferences import EbayPreferences, SellerProfileType
+from inventorum.ebay.lib.ebay.preferences import EbayPreferences
 from inventorum.ebay.tests import EbayTest
 from inventorum.ebay.tests.testcases import EbayAuthenticatedAPITestCase
 
@@ -19,7 +20,7 @@ class TestEbayPreferences(EbayAuthenticatedAPITestCase):
         supported_seller_profiles = user_preferences.seller_profile_preferences.supported_seller_profiles
         self.assertTrue(len(supported_seller_profiles) > 0, 'There should be at least one configured seller profile')
 
-        return_profiles = [p for p in supported_seller_profiles if p.profile_type == SellerProfileType.RETURN_POLICY
+        return_profiles = [p for p in supported_seller_profiles if p.profile_type == SellerProfileCodeType.RETURN_POLICY
                            and p.category_group.is_default]
         self.assertTrue(len(return_profiles) > 0, 'There should be at least one default return policy profile')
 
