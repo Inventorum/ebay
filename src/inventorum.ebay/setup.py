20dd0c4102cbd1d1d7e4a70ee493444c1418043e
Eddy Ernesto del Valle Pino <eddy@inventorum.com>
2016-01-12 12:17:33 +0100

    Rewrite the python setup.py.

--- src/inventorum.ebay/setup.py
+++ src/inventorum.ebay/setup.py
@@ -5,44 +5,32 @@ import os
 here = os.path.abspath(os.path.dirname(__file__))
 VERSION = open(os.path.join(here, 'VERSION')).read().strip()
 
-entry_points = """\
-[paste.app_factory]
-ebay_main = inventorum.util.paste:make_wsgi_application
-
-[console_scripts]
-celery = inventorum.ebay.scripts.celery:run
-provisioning/provision_db = inventorum.ebay.scripts.provisioning:provision_db
-provisioning/provision_rabbitmq = inventorum.ebay.scripts.provisioning:provision_rabbitmq
-manage = inventorum.util.paste:manage
-migrate_from_old_ebay = inventorum.ebay.scripts.migration_tool:migrate_from_old_ebay
-"""
-
 # alphabetically ordered(!)
 required_eggs = [
-    'celery>=3.1.17',
-    'ebaysdk>=2.1.1-dev4',
-    'flower>=0.8.1',
-    'grequests>=0.3.1',
+    'celery==3.1.18',
+    'ebaysdk==2.1.1-dev4',
+    'flower==0.8.1',
+    'grequests==0.3.1',
 
-    'graypy>=0.2.11',
+    'graypy==0.2.11',
 
-    'Django>=1.7.7',
-    'django-countries>=3.3',
-    'django-extensions>=1.5.2',
-    'djangorestframework==3.1.3',
-    'django-mptt>=0.6.1',
+    'Django==1.7.9',
+    'django-countries==3.3',
+    'django-extensions==1.5.2',
+    'djangorestframework==2.3.14',
+    'django-mptt==0.6.1',
     'django-pastedeploy-settings>=1.0rc4dev',
-    'django-rest-swagger>=0.2.9',
+    'django-rest-swagger==0.2.9',
 
-    'inventorum.util==10.0.0',
+    'inventorum.util>=10.0.0',
 
-    'mock>=1.0.1',
-    'plac>=0.9.1',
-    'python-keyczar>=0.715',
-    'raven>=5.2.0',
-    'requests>=2.6.0',
-    'vcrpy>=1.3.0',
-    'waitress>=0.8.9',
+    'mock==1.0.1',
+    'plac==0.9.1',
+    'python-keyczar==0.715',
+    'raven==5.2.0',
+    'requests==2.6.0',
+    'vcrpy==1.3.0',
+    'waitress==0.8.9',
 ]
 
 setup(
@@ -54,22 +42,33 @@ setup(
     url='',
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     include_package_data=True,
-    package_data={'inventorum': [], 'static': [],},
+    package_data={'inventorum.ebay': [], 'static': []},
     scripts=[],
     install_requires=required_eggs,
     extras_require=dict(
         # alphabetically ordered
         test=required_eggs + [
-            'coverage>=3.7',
-            'django-nose>=1.3',
-            'factory_boy>=2.5',
-            'mock>=1.0.1'
+            'coverage==3.7',
+            'django-nose==1.3',
+            'factory_boy==2.5',
+            'mock==1.0.1'
         ]
     ),
     # http://pythonhosted.org/distribute/setuptools.html#namespace-packages
     namespace_packages=['inventorum'],
     zip_safe=False,
-    entry_points=entry_points,
+    entry_points={
+        'paste.app_factory': [
+            'ebay_main = inventorum.util.paste:make_wsgi_application',
+        ],
+        'console_scripts': {
+            'celery = inventorum.ebay.scripts.celery:run',
+            'provision_db = inventorum.ebay.scripts.provisioning:provision_db',
+            'provision_rabbitmq = inventorum.ebay.scripts.provisioning:provision_rabbitmq',
+            'manage = inventorum.util.paste:manage',
+            'migrate_from_old_ebay = inventorum.ebay.scripts.migration_tool:migrate_from_old_ebay',
+        }
+    },
     dependency_links=[
         # use the last commit-id in our branch that we rely on
         'https://github.com/bimusiek/ebaysdk-python/archive/master.zip'
