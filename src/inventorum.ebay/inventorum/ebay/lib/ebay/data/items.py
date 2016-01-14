a9313cbd1950d485bad04da322b1c3aacfee81ba
Eddy Ernesto del Valle Pino <eddy@inventorum.com>
2016-01-14 16:27:14 +0100

    Change UseTaxTable -> ProductDetails.PictureURL.

--- src/inventorum.ebay/inventorum/ebay/lib/ebay/data/items.py
+++ src/inventorum.ebay/inventorum/ebay/lib/ebay/data/items.py
@@ -217,7 +217,7 @@ class EbayFixedPriceItem(object):
             'PayPalEmailAddress': self.paypal_email_address,
             'PaymentMethods': self.payment_methods,
             'PrimaryCategory': {'CategoryID': self.category_id},
-            'UseTaxTable': True,  # mark the product like taxes were calculated by the seller.
+            'VatDetails': {'VatPercent': 1},  # mark the product like taxes were calculated by the seller.
         }
         if self.pictures:
             data['PictureDetails'] = {'PictureURL': [p.url for p in self.pictures]}
