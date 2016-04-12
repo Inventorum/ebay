b457c9fc0c02aebddc21514a9e06839b56022625
Ernesto Avilés Vázquez <ernesto@inventorum.com>
2016-04-12 17:41:34 +0200

    Test item delay publishing marked as failed

---
+++ src/inventorum.ebay/inventorum/ebay/apps/products/tests/test_delayed_items_publishing_finalization.py
@@ -0,0 +1,58 @@
+# encoding: utf-8
+from __future__ import absolute_import, unicode_literals
+
+import logging
+import mock
+from datetime import datetime
+from datetime import timedelta
+from time import sleep
+
+import mock
+
+from inventorum.ebay.lib.celery import get_anonymous_task_execution_context
+from inventorum.ebay.tests.testcases import UnitTestCase
+from ...accounts.tests.factories import EbayUserFactory
+from ...products.tasks import periodic_ebay_timeouted_item_check_task
+from ...products.tests.factories import EbayProductFactory
+from ...products import EbayItemPublishingStatus
+from ...products.models import EbayItemModel
+from .factories import EbayItemFactory
+
+
+log = logging.getLogger(__name__)
+
+
+class TestDelayedItemsPublishingGetsFinalized(UnitTestCase):
+
+    def setUp(self):
+        super(TestDelayedItemsPublishingGetsFinalized, self).setUp()
+        self.user = EbayUserFactory.create()
+        self.account = self.user.account
+        self.product = EbayProductFactory.create(account=self.account)
+        self.timeout = 5
+
+        self._make_items()
+        sleep(self.timeout)
+        self._make_items()
+
+    def _make_items(self, count=5):
+        for counter in range(count):
+            item = EbayItemFactory.create(account=self.account,
+                                          product=self.product,
+                                          publishing_status=EbayItemPublishingStatus.IN_PROGRESS)
+
+    @mock.patch('inventorum.ebay.apps.products.tasks._finalize_ebay_item_publish.delay')
+    def test_delayed_publishing_made_failed(self, finalize_ebay_item_publish_delay_mock):
+        kwargs = dict(
+            timeout=self.timeout,
+            context=get_anonymous_task_execution_context())
+
+        periodic_ebay_timeouted_item_check_task.apply(kwargs=kwargs)
+
+        failed_items = EbayItemModel.objects.filter(
+            publishing_status=EbayItemPublishingStatus.FAILED)
+
+        self.assertEqual(failed_items.count(), 5)
+        self.assertEqual(failed_items.first().publishing_status_details,
+                         dict(message='Publishing timeout ({} seconds).'.format(self.timeout)))
+        self.assertEqual(finalize_ebay_item_publish_delay_mock.call_count, 5)
